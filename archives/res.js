6138fa284354773ab7cd0b9fb36065fd5723505f
gk7995 <ghkhatri7@gmail.com>
2022-01-27 17:23:30 +0530

    empty

---
+++ archives/res.js
@@ -0,0 +1,240 @@
+function s1(){
+    var element = document.getElementById("s1");
+    element.classList.toggle("hide");
+}
+function s2(){
+    var element = document.getElementById("s2");
+    element.classList.toggle("hide");
+}
+function s3(){
+    var element = document.getElementById("s3");
+    element.classList.toggle("hide");
+}
+function s4(){
+    var element = document.getElementById("s4");
+    element.classList.toggle("hide");
+}
+function s5(){
+    var element = document.getElementById("s5");
+    element.classList.toggle("hide");
+}
+function s6(){
+    var element = document.getElementById("s6");
+    element.classList.toggle("hide");
+}
+function s7(){
+    var element = document.getElementById("s7");
+    element.classList.toggle("hide");
+}
+function s8(){
+    var element = document.getElementById("s8");
+    element.classList.toggle("hide");
+}
+function s9(){
+    var element = document.getElementById("s9");
+    element.classList.toggle("hide");
+}
+function s10(){
+    var element = document.getElementById("s10");
+    element.classList.toggle("hide");
+}
+function s11(){
+    var element = document.getElementById("s11");
+    element.classList.toggle("hide");
+}
+function s12(){
+    var element = document.getElementById("s12");
+    element.classList.toggle("hide");
+}
+function s13(){
+    var element = document.getElementById("s13");
+    element.classList.toggle("hide");
+}
+function s14(){
+    var element = document.getElementById("s14");
+    element.classList.toggle("hide");
+}
+function s15(){
+    var element = document.getElementById("s15");
+    element.classList.toggle("hide");
+}
+function s16(){
+    var element = document.getElementById("s16");
+    element.classList.toggle("hide");
+}
+function s17(){
+    var element = document.getElementById("s17");
+    element.classList.toggle("hide");
+}
+function s18(){
+    var element = document.getElementById("s18");
+    element.classList.toggle("hide");
+}
+function s19(){
+    var element = document.getElementById("s19");
+    element.classList.toggle("hide");
+}
+function s20(){
+    var element = document.getElementById("s20");
+    element.classList.toggle("hide");
+}
+function s21(){
+    var element = document.getElementById("s21");
+    element.classList.toggle("hide");
+}
+function s22(){
+    var element = document.getElementById("s22");
+    element.classList.toggle("hide");
+}
+function s23(){
+    var element = document.getElementById("s23");
+    element.classList.toggle("hide");
+}
+function s24(){
+    var element = document.getElementById("s24");
+    element.classList.toggle("hide");
+}
+function s25(){
+    var element = document.getElementById("s25");
+    element.classList.toggle("hide");
+}
+function s26(){
+    var element = document.getElementById("s26");
+    element.classList.toggle("hide");
+}
+function s27(){
+    var element = document.getElementById("s27");
+    element.classList.toggle("hide");
+}
+function s28(){
+    var element = document.getElementById("s28");
+    element.classList.toggle("hide");
+}
+function s29(){
+    var element = document.getElementById("s29");
+    element.classList.toggle("hide");
+}
+function s30(){
+    var element = document.getElementById("s30");
+    element.classList.toggle("hide");
+}
+function s31(){
+    var element = document.getElementById("s31");
+    element.classList.toggle("hide");
+}
+function s32(){
+    var element = document.getElementById("s32");
+    element.classList.toggle("hide");
+}
+function s33(){
+    var element = document.getElementById("s33");
+    element.classList.toggle("hide");
+}
+function s34(){
+    var element = document.getElementById("s34");
+    element.classList.toggle("hide");
+}
+function s35(){
+    var element = document.getElementById("s35");
+    element.classList.toggle("hide");
+}
+function s36(){
+    var element = document.getElementById("s36");
+    element.classList.toggle("hide");
+}
+function s37(){
+    var element = document.getElementById("s37");
+    element.classList.toggle("hide");
+}
+function s38(){
+    var element = document.getElementById("s38");
+    element.classList.toggle("hide");
+}
+function s39(){
+    var element = document.getElementById("s39");
+    element.classList.toggle("hide");
+}
+function s40(){
+    var element = document.getElementById("s40");
+    element.classList.toggle("hide");
+}
+function s41(){
+    var element = document.getElementById("s41");
+    element.classList.toggle("hide");
+}
+function s42(){
+    var element = document.getElementById("s42");
+    element.classList.toggle("hide");
+}
+function s43(){
+    var element = document.getElementById("s43");
+    element.classList.toggle("hide");
+}
+function s44(){
+    var element = document.getElementById("s44");
+    element.classList.toggle("hide");
+}
+function s45(){
+    var element = document.getElementById("s45");
+    element.classList.toggle("hide");
+}
+function s46(){
+    var element = document.getElementById("s46");
+    element.classList.toggle("hide");
+}
+function s47(){
+    var element = document.getElementById("s47");
+    element.classList.toggle("hide");
+}
+function s48(){
+    var element = document.getElementById("s48");
+    element.classList.toggle("hide");
+}
+function s49(){
+    var element = document.getElementById("s49");
+    element.classList.toggle("hide");
+}
+function s50(){
+    var element = document.getElementById("s50");
+    element.classList.toggle("hide");
+}
+function s51(){
+    var element = document.getElementById("s51");
+    element.classList.toggle("hide");
+}
+function s52(){
+    var element = document.getElementById("s52");
+    element.classList.toggle("hide");
+}
+function s53(){
+    var element = document.getElementById("s53");
+    element.classList.toggle("hide");
+}
+function s54(){
+    var element = document.getElementById("s54");
+    element.classList.toggle("hide");
+}
+function s55(){
+    var element = document.getElementById("s55");
+    element.classList.toggle("hide");
+}
+function s56(){
+    var element = document.getElementById("s56");
+    element.classList.toggle("hide");
+}
+function s57(){
+    var element = document.getElementById("s57");
+    element.classList.toggle("hide");
+}
+function s58(){
+    var element = document.getElementById("s58");
+    element.classList.toggle("hide");
+}
+function s59(){
+    var element = document.getElementById("s59");
+    element.classList.toggle("hide");
+}
+function s60(){
+    var element = document.getElementById("s60");
+    element.classList.toggle("hide");
+}
